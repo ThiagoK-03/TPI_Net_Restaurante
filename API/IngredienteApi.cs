a1d7aad78079de8c52e780c9936c58a5cc4ef2cd
ThiagoA <thiagoabbate@gmail.com>
2025-10-25 19:33:06 -0300

    feat: API creation

---
+++ API/IngredienteApi.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+using DTOs;
+
+namespace API
+{
+    public class IngredienteApi
+    {
+        private static HttpClient client = new HttpClient();
+        static IngredienteApi()
+        {
+            client.BaseAddress = new Uri("http://localhost:5205/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+
+        public static async Task<IngredienteDTO> GetAsync(int id)
+        {
+            IngredienteDTO ingrediente = null;
+            HttpResponseMessage response = await client.GetAsync("ingredientes/" + id);
+            if (response.IsSuccessStatusCode)
+            {
+                ingrediente = await response.Content.ReadFromJsonAsync<IngredienteDTO>();
+            }
+            return ingrediente;
+        }
+
+        public static async Task<IEnumerable<IngredienteDTO>> GetAllAsync()
+        {
+            IEnumerable<IngredienteDTO> ingredientes = null;
+            HttpResponseMessage response = await client.GetAsync("ingredientes");
+            if (response.IsSuccessStatusCode)
+            {
+                ingredientes = await response.Content.ReadFromJsonAsync<IEnumerable<IngredienteDTO>>();
+            }
+            return ingredientes;
+        }
+
+        public async static Task AddAsync(IngredienteDTO ingrediente)
+        {
+            HttpResponseMessage response = await client.PostAsJsonAsync("ingredientes", ingrediente);
+            response.EnsureSuccessStatusCode();
+        }
+
+        public static async Task DeleteAsync(int id)
+        {
+            HttpResponseMessage response = await client.DeleteAsync("ingredientes/" + id);
+            response.EnsureSuccessStatusCode();
+        }
+
+        public static async Task UpdateAsync(IngredienteDTO ingrediente)
+        {
+            HttpResponseMessage response = await client.PutAsJsonAsync("ingredientes", ingrediente);
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
---
+++ API/ProveedorApi.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+using DTOs;
+
+namespace API
+{
+    public class ProveedorApi
+    {
+        private static HttpClient client = new HttpClient();
+        static ProveedorApi()
+        {
+            client.BaseAddress = new Uri("http://localhost:5205/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+
+        public static async Task<ProveedorDTO> GetAsync(int id)
+        {
+            ProveedorDTO proveedor = null;
+            HttpResponseMessage response = await client.GetAsync("proveedores/" + id);
+            if (response.IsSuccessStatusCode)
+            {
+                proveedor = await response.Content.ReadFromJsonAsync<ProveedorDTO>();
+            }
+            return proveedor;
+        }
+
+        public static async Task<IEnumerable<ProveedorDTO>> GetAllAsync()
+        {
+            IEnumerable<ProveedorDTO> proveedores = null;
+            HttpResponseMessage response = await client.GetAsync("proveedores");
+            if (response.IsSuccessStatusCode)
+            {
+                proveedores = await response.Content.ReadFromJsonAsync<IEnumerable<ProveedorDTO>>();
+            }
+            return proveedores;
+        }
+
+        public async static Task AddAsync(ProveedorDTO proveedor)
+        {
+            HttpResponseMessage response = await client.PostAsJsonAsync("proveedores", proveedor);
+            response.EnsureSuccessStatusCode();
+        }
+
+        public static async Task DeleteAsync(int id)
+        {
+            HttpResponseMessage response = await client.DeleteAsync("proveedores/" + id);
+            response.EnsureSuccessStatusCode();
+        }
+
+        public static async Task UpdateAsync(ProveedorDTO proveedor)
+        {
+            HttpResponseMessage response = await client.PutAsJsonAsync("proveedores", proveedor);
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
